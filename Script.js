b504b2612a52e5273989f9158993a90d1c909e80
aravindhan.palanisamy <aravindhan.palanisamy@solitontech.com>
2023-02-21 14:53:14 +0530

    feat: nodejs: Update Weather List every one hour

--- Script.js
+++ Script.js
@@ -703,5 +703,5 @@ const asyncAwait = async () => {
 (function () {
   asyncAwait();
   clearInterval(weatherListInterval);
-  weatherListInterval = setInterval(asyncAwait, 10000);
+  weatherListInterval = setInterval(asyncAwait, 3600000);
 })();
