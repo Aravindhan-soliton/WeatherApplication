4e5d59ba6fafbe8e70fd72695b491d2abea1cc14
aravindhan.palanisamy <aravindhan.palanisamy@solitontech.com>
2023-02-24 11:34:06 +0530

    feat: nodejs: update the code with event emitters

--- server.js
+++ server.js
@@ -13,52 +13,70 @@ var app = express();
 app.use(express.json());
 
 const { fork } = require("child_process");
+const EventEmitter = require("events");
+var eventEmitter = new EventEmitter();
 
-// send all cities timeZone
-app.get("/all-timezone-cities", function (req, res) {
+eventEmitter.on("allTime", (res, messageName) => {
   // create a Child process using fork
   let allTimezon = fork(__dirname + "/child.js");
   allTimezon.on("message", (weatherinfo) => {
     lastForeCast = weatherinfo;
     res.json(weatherinfo);
   });
-  allTimezon.send({ messagename: "GetData", messagebody: {} });
+  allTimezon.send({ messagename: messageName, messagebody: {} });
 });
 
-//send a city time to
-app.get("/", function (req, res) {
-  let city = req.query.city;
-  if (city) {
-    // create a Child process using fork
-    let cityInfo = fork(__dirname + "/child.js");
-    cityInfo.on("message", (cityData) => {
-      res.json(cityData);
-    });
-    cityInfo.send({
-      messagename: "GetcityInfo",
-      messagebody: { cityname: city },
-    });
-  } else if (req.url.endsWith("/")) {
-    app.use(express.static("./"));
-    res.sendFile(__dirname + "/index.html");
-  }
+eventEmitter.on("cityData", (res, messageName, city) => {
+  // create a Child process using fork
+  let cityInfo = fork(__dirname + "/child.js");
+  cityInfo.on("message", (cityData) => {
+    res.json(cityData);
+  });
+  cityInfo.send({
+    messagename: messageName,
+    messagebody: { cityname: city },
+  });
 });
 
-//send hourly forecast for one city
-app.post("/hourly-forecast", function (req, res) {
+eventEmitter.on("indexLoad", (res) => {
+  app.use(express.static("./"));
+  res.sendFile(__dirname + "/index.html");
+});
+
+eventEmitter.on("nextFiveHoursData", (res, req, messageName) => {
   // create a Child process using fork
   let temperature = fork(__dirname + "/child.js");
   temperature.on("message", (nextFiveHrs) => {
     res.json(nextFiveHrs);
   });
   temperature.send({
-    messagename: "GetTemperature",
+    messagename: messageName,
     messagebody: {
       cityDTN: req.body.city_Date_Time_Name,
-      hours: "6",
+      hours: req.body.hours,
       weatherData: lastForeCast,
     },
   });
+});
+
+// send all cities timeZone
+app.get("/all-timezone-cities", function (req, res) {
+  eventEmitter.emit("allTime", res, "GetData");
+});
+
+//send a city time to
+app.get("/", function (req, res) {
+  let city = req.query.city;
+  if (city) {
+    eventEmitter.emit("cityData", res, "GetcityInfo", city);
+  } else if (req.url.endsWith("/")) {
+    eventEmitter.emit("indexLoad", res);
+  }
+});
+
+//send hourly forecast for one city
+app.post("/hourly-forecast", function (req, res) {
+  eventEmitter.emit("nextFiveHoursData", res, req, "GetTemperature");
 });
 
 const server = http.createServer(app);
